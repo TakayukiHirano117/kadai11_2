9d36a29678615fa252f29a4917c71fbedf5f07d2
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2023-03-14 17:57:29 +0900

    Create raikou.java

---
+++ raikou.java
@@ -0,0 +1,10 @@
+package kadai_11;
+
+public class raikou implements ThunderPokemon {
+	private String name = "Raikou";
+	
+	public void thunderAttack() {
+		System.out.println(this.name + "の雷属性の攻撃です。");
+	}
+	
+}
