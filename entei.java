6a2de897c5d28e9a8a45f6cf619dcb4dff659205
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2023-03-14 17:57:41 +0900

    Create entei.java

---
+++ entei.java
@@ -0,0 +1,10 @@
+package kadai_11;
+
+public class entei implements FirePokemon {
+	private String name = "Entei";
+	
+	public void fireAttack() {
+		System.out.println(this.name + "の炎属性の攻撃です。");
+	}
+	
+}
